fbdaec6d3546a5de3aedbe1abf5acca7c1af8948
tfanen <tfanenprint@gmail.com>
2026-09-23 21:03:45 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+// Security Headers
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  next();
+});
+
 // IN-MEMORY HIGH-SPEED CACHE & ATOMIC NON-BLOCKING WRITE QUEUE
 let dbCache = null;
 let isWriting = false;
@@ -1104,6 +1112,11 @@ app.post('/api/quotes', (req, res) => {
 
 // Serve static uploaded images & static web files
 app.use('/uploads', express.static(UPLOADS_DIR));
+
+app.get('/robots.txt', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.sendFile(path.join(__dirname, 'public', 'robots.txt'));
+});
 
 app.get('/sitemap.xml', (req, res) => {
   res.setHeader('Content-Type', 'application/xml');
